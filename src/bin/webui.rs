d7356167bbe436861a43fcabe66c298a222a62a9
Katrina Brock <keb266@cornell.edu>
2017-08-20 18:20:24 -0700

    Move show_posts to webui

--- src/bin/webui.rs
+++ src/bin/webui.rs
@@ -1,13 +1,39 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate diesel_demo;
+extern crate diesel;
 extern crate rocket;
+
+use diesel_demo::*;
+use self::models::*;
+use diesel::prelude::*;
+
 
 #[cfg(test)] mod tests;
 
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn hello() -> String {
+    use self::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    let postcount = format!("Displaying {} posts", results.len());
+    let mut current_posts = String::from("");
+    for post in results {
+        current_posts.push_str(&post.title);
+        current_posts.push_str(&"-----------\n".to_string());
+        current_posts.push_str(&post.body);
+    }
+    
+    let content = format!("{}.\n{}", postcount, current_posts);
+
+    content
 }
 
 fn main() {
