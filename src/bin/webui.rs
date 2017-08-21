dd9ada4b230e333a31ca2d03b01f6548520eb2a2
Katrina Brock <keb266@cornell.edu>
2017-08-20 17:32:20 -0700

    Add rocket Helloworld to webui

--- src/bin/webui.rs
+++ src/bin/webui.rs
@@ -1,3 +1,15 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[cfg(test)] mod tests;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![hello]).launch();
 }
